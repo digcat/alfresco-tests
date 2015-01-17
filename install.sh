1657597a0eddb9714b38c6d9286d4474beb58d95
digcat <ingo@digcat.com>
2015-01-17 09:38:43 +0000

    downgrade cmislib to 0.51 so test passes

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@ sudo apt-get install xvfb x11-xkb-utils
 sudo apt-get install xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps
 sudo apt-get install python-yaml
 sudo apt-get install firefox
+sudo easy_install cmislib  ### uses 0.51 cmislib
 sudo pip install selenium
-sudo pip install cmislib
+sudo pip install configure
+sudo pip install configuration
+sudo apt-get update
+sudo apt-get upgrade
--- test_cmis.py
+++ test_cmis.py
@@ -1,5 +1,6 @@
 import socket
 import unittest
+import cmislib
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from configure import Configuration
