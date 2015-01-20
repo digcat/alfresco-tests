b987e00e6bb128fc1564a1f9e1a8bb67a9538834
digcat <info@digcat.com>
2015-01-20 08:06:24 +0000

    move cmis test to start, as this waits until alfresco is fully up, ftp doesnt

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,9 @@
-xvfb-run python test_ftp.py
-sleep 1
 xvfb-run python test_cmis.py
+sleep 1
+xvfb-run python test_ftp.py
 sleep 1
 xvfb-run python test_ex_swsdp.py
 sleep 1
+xvfb-run python test_swsdp.py
 
 
