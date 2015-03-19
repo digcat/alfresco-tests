f052c760eb43a0571c85ddb06b25b6bc5ef7c8d7
darenf <daren@digcat.com>
2015-03-19 08:37:46 +0000

    set DISPLAY variable

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,7 @@
 set -e
-xvfb-run python test_share.py
-xvfb-run python test_rm.py
-xvfb-run python test_jsconsole.py
+export DISPLAY=:0.0 
+sudo xvfb-run python test_share.py
+sudo xvfb-run python test_rm.py
+sudo xvfb-run python test_jsconsole.py
 #xvfb-run python test_services.py
 #xvfb-run python test_share_site_creators.py
