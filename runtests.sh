6b3de3822985aa55d4dfcd5aeaf53e944c715872
darenf <daren@digcat.com>
2015-03-19 15:48:32 +0000

    try to exit on error

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,10 @@
 set -e
 export DISPLAY=:0.0 
 sudo xvfb-run python test_share.py
+if [ $0 != 0 ]; then exit 99; fi
 sudo xvfb-run python test_rm.py
+if [ $0 != 0 ]; then exit 99; fi
 sudo xvfb-run python test_jsconsole.py
+if [ $0 != 0 ]; then exit 99; fi
 #xvfb-run python test_services.py
 #xvfb-run python test_share_site_creators.py
