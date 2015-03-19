93ddebcf9bf6802778f083d7fda612698955fed6
darenf <daren@digcat.com>
2015-03-19 19:33:09 +0000

    disable phantomjs

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ sudo apt-get install python
 sudo apt-get install python-yaml
 sudo pip install configure
 sudo pip install configuration
-sudo apt-get install nodejs
-sudo apt-get install npm
-sudo npm -g install phantomjs
+#sudo apt-get install nodejs
+#sudo apt-get install npm
+#sudo npm -g install phantomjs
 sudo pip install selenium
 sudo apt-get install xvfb x11-xkb-utils 
 sudo apt-get install xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps
 sudo easy_install cmislib  ### uses 0.51 cmislib
 sudo apt-get install firefox
-export PHANTOMJS=/usr/local/bin/phantomjs
+#export PHANTOMJS=/usr/local/bin/phantomjs
