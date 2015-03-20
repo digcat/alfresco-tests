691a3feedf54c5581c658d0ae34a013a1015154a
darenf <daren@digcat.com>
2015-03-20 11:34:40 +0000

    add marsbard change

--- runtests.sh
+++ runtests.sh
@@ -1,10 +1,15 @@
-set -e
-export DISPLAY=:0.0 
-sudo xvfb-run -e /dev/stdout -a python test_share.py
-if [ $0 != 0 ]; then exit 99; fi
-sudo xvfb-run -a python test_rm.py
-if [ $0 != 0 ]; then exit 99; fi
-sudo xvfb-run -a python test_jsconsole.py
-if [ $0 != 0 ]; then exit 99; fi
-#xvfb-run python test_services.py
-#xvfb-run python test_share_site_creators.py
+#!/bin/bash
+trap 'exit 9' ERR
+set -x
+CMD="sudo xvfb-run -e /dev/stdout python"
+
+#TESTS="share rm jsconsole services share_site_creators"
+TESTS="share rm jsconsole"
+
+for t in $TESTS
+do
+  $CMD test_$t.py
+  # actually the trap line at top should handle this
+  # if errorlevel==9 then the trap got it, if 99 then this
+  if [ $0 != 0 ]; then exit 99; fi
+done
