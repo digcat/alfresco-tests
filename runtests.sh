ea6d9b1f8507bc92d6989eace9d0b91a48bf2e35
darenf <daren@digcat.com>
2015-03-20 10:26:52 +0000

    use alternative display if available

--- runtests.sh
+++ runtests.sh
@@ -1,10 +1,10 @@
 set -e
 export DISPLAY=:0.0 
-sudo xvfb-run python test_share.py
+sudo xvfb-run -a python test_share.py
 if [ $0 != 0 ]; then exit 99; fi
-sudo xvfb-run python test_rm.py
+sudo xvfb-run -a python test_rm.py
 if [ $0 != 0 ]; then exit 99; fi
-sudo xvfb-run python test_jsconsole.py
+sudo xvfb-run -a python test_jsconsole.py
 if [ $0 != 0 ]; then exit 99; fi
 #xvfb-run python test_services.py
 #xvfb-run python test_share_site_creators.py
