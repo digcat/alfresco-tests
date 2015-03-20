25b86c07f13e0b207642e7b449880be73a9f48c1
darenf <daren@digcat.com>
2015-03-20 13:32:06 +0000

    reorder packages

--- install.sh
+++ install.sh
@@ -1,15 +1,10 @@
 set -e
-#DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 sudo apt-get install python
 sudo apt-get install python-yaml
 sudo pip install configure
 sudo pip install configuration
-#sudo apt-get install nodejs
-#sudo apt-get install npm
-#sudo npm -g install phantomjs
-sudo pip install selenium
 sudo apt-get install xvfb x11-xkb-utils 
 sudo apt-get install xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps
 sudo easy_install cmislib  ### uses 0.51 cmislib
+sudo pip install selenium
 sudo apt-get install firefox
-#export PHANTOMJS=/usr/local/bin/phantomjs
