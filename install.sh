a3401e31ff4abe28d18737915826f5a8077d8e1c
darenf <daren@digcat.com>
2015-03-21 09:48:42 +0000

    fix package load for use on vm

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 set -e
 sudo apt-get -y install python
+sudo apt-get -y install python-pip
 sudo apt-get -y install python-yaml
 sudo pip install configure
 sudo pip install configuration
 sudo apt-get -y install xvfb x11-xkb-utils 
-sudo apt-get -y install x-ttcidfont-conf 
 sudo apt-get -y install xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps
 sudo easy_install cmislib  ### uses 0.51 cmislib
 sudo pip install selenium
