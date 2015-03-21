0329e2d80dec0383cf9ef6bf873f57ee23564cfc
darenf <daren@digcat.com>
2015-03-21 09:52:20 +0000

    remove debug from Xvfb

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 trap 'exit 9' ERR
 set -x
-CMD="sudo xvfb-run -a -e /dev/stdout python"
+CMD="sudo xvfb-run -a python"
 
 #TESTS="share rm jsconsole services share_site_creators"
 TESTS="share rm jsconsole"
