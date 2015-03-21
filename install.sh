174fe62b6a5c30166da6b8ac77b6c2f6a92be86a
darenf <daren@digcat.com>
2015-03-21 04:28:59 +0000

    force -y on apt install

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 set -e
-sudo apt-get install python
-sudo apt-get install python-yaml
+sudo apt-get -y install python
+sudo apt-get -y install python-yaml
 sudo pip install configure
 sudo pip install configuration
-sudo apt-get install xvfb x11-xkb-utils 
-sudo apt-get install xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps
+sudo apt-get -y install xvfb x11-xkb-utils 
+sudo apt-get -y install x-ttcidfont-conf 
+sudo apt-get -y install xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps
 sudo easy_install cmislib  ### uses 0.51 cmislib
 sudo pip install selenium
-sudo apt-get install firefox
+sudo apt-get -y install firefox
