979e0c4666659cfd3700d6580d9defdb62d1c7b1
darenf <daren@digcat.com>
2015-03-31 10:41:04 +0100

    build virtualenv and runtests

--- install.sh
+++ install.sh
@@ -1,11 +1,37 @@
-set -e
-sudo apt-get -y install python
-sudo apt-get -y install python-pip
-sudo apt-get -y install python-yaml
-sudo pip install configure
-sudo pip install configuration
-sudo apt-get -y install xvfb x11-xkb-utils 
-sudo apt-get -y install xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps
-sudo easy_install cmislib  ### uses 0.51 cmislib
-sudo pip install selenium
-sudo apt-get -y install firefox
+#!/bin/bash
+
+ROOT=`pwd`
+
+if [ -e config.yml ] ; then
+	echo "Found local config.yml copy into test environment"
+fi
+
+if [ -e testing_virt ] ; then
+	echo "Deleting Testing Virtual"
+	sudo rm -rf testing_virt
+fi
+
+if uname -a | grep "Ubuntu" ; then
+	echo "Ubuntu"
+	sudo apt-get install libyaml-dev -y
+	sudo apt-get install python-yaml -y
+	sudo apt-get install git -y
+fi
+if uname -a | grep "Centos" ; then
+	echo "Centos"
+	sudo yum install libyaml-dev -y
+	sudo yum install python-yaml -y
+	sudo yum install git -y
+fi
+
+echo "Build Virtual Environment"
+pip install virtualenv --upgrade
+mkdir testing_virt ; cd testing_virt
+virtualenv venv
+source venv/bin/activate
+venv/bin/pip install selenium --upgrade
+venv/bin/pip install configure --upgrade
+venv/bin/easy_install PyYAML
+cd $ROOT
+./runtests.sh ./testing_virt/venv/bin
+
