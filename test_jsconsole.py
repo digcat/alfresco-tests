1fab52630ea880df6f1c6b93ea5a003b137b8d84
darenf <daren@digcat.com>
2015-08-03 13:00:57 +0100

    Remove tabs

--- test_jsconsole.py
+++ test_jsconsole.py
@@ -13,23 +13,23 @@ class testJSConsoleMyAlfresco(unittest.TestCase):
 
     def setUp(self):
         """ Setup browser and connection """
-	self = testconfig.getVars(self)
-	self.driver = testconfig.setBrowser(self)
+        self = testconfig.getVars(self)
+        self.driver = testconfig.setBrowser(self)
         self.driver.get(self.loginurl)
 
     def test_JSConsole_alfresco_login(self):
         main_page = page.MainPage(self.driver)
-	main_page.click_login_button()
-	main_page.photo_page()
+        main_page.click_login_button()
+        main_page.photo_page()
         assert main_page.is_title_matches("Dashboard"),"FAIL: didnt make it to dashboard on login ! Check Credentials"
 	
     def test_JSConsole_goto_jsconsole(self):
-	jsconsole_page = page_jsconsole.JSConsolePage(self.driver)	
-	jsconsole_page.click_login_button()
-	jsconsole_page.click_jsconsole_button()
+        jsconsole_page = page_jsconsole.JSConsolePage(self.driver)	
+        jsconsole_page.click_login_button()
+        jsconsole_page.click_jsconsole_button()
         main_page = page.MainPage(self.driver)
-	main_page.photo_page()
-	assert main_page,is_title_matches("Admin Tools", "FAIL: to reach admin tools page")
+        main_page.photo_page()
+        assert main_page,is_title_matches("Admin Tools", "FAIL: to reach admin tools page")
 
     def tearDown(self):
         self.driver.close()
