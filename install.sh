990dc6df87a109f7f24da146d436a0240f091062
Martin Cosgrave <martin@ciderpunk.com>
2016-05-11 12:42:15 +0200

    fix OS detection

--- install.sh
+++ install.sh
@@ -14,11 +14,11 @@ if [ -e testing_virt ] ; then
 fi
 
 
-if -f /etc/debian_version ; then
+if [ -f /etc/debian_version ] ; then
 	echo "Ubuntu"
 	sudo apt-get install -y gcc python-dev libyaml-dev python-yaml git python-pip xvfb firefox
 fi
-if -f /etc/redhat-release ; then
+if [ -f /etc/redhat-release ] ; then
 	echo "Centos"
 	sudo yum install -y gcc python-dev libyaml-dev python-yaml git python-pip xvfb firefox
 fi
