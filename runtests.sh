7691c2d01381f0a0c494843208522cf47b434f44
Martin Cosgrave <martin@ciderpunk.com>
2016-05-11 11:19:50 +0200

    instead of failing out, write status files to /tmp/testres

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-trap 'exit 9' ERR
+
 set -x
 args=("$@")
 PYTH=${args[0]}
@@ -10,10 +10,15 @@ CMD="xvfb-run -a $PYTH/python"
 #TESTS="share rm jsconsole services share_site_creators"
 TESTS="share jsconsole cmis preview"
 
+rm -rf /tmp/testres
+mkdir -p /tmp/testres
 for t in $TESTS
 do
   $CMD test_$t.py
-  # actually the trap line at top should handle this
-  # if errorlevel==9 then the trap got it, if 99 then this
-  if [ $? != 0 ]; then exit 99; fi
+  if [ $? != 0 ] 
+	then 
+		touch /tmp/testres/${t}_FAIL
+	else
+		touch /tmp/testres/${t}_PASS
+	fi
 done
